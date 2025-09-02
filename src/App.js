818b3995ad6dd98d6d9278f402cf5ceee1436002
boykin345 <juzzyboykin@gmail.com>
2025-09-02 15:54:14 +0500

    [.d] Style borders added

--- src/App.js
+++ src/App.js
@@ -2,54 +2,90 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
+    <section>
       <h1>Prompt Generator</h1>
       <div>
-        <textarea name="" id="" rows={5} cols={40}></textarea>
+        <div className="rawtextarea-div">
+          <div>
+            <textarea
+              name=""
+              placeholder="Write your text here."
+              id=""
+              rows={5}
+              cols={40}
+            ></textarea>
+          </div>
+          <br />
+          <div>
+            <label htmlFor="">Model: </label>
+            <select name="" id="">
+              <option value="midjourney">Midjourney</option>
+              <option value="comfyui">ComfyUI</option>
+              <option value="dalle">DALL-E</option>
+            </select>
+          </div>
+          <br />
+          <div>
+            <label htmlFor="">Style: </label>
+            <select name="" id="">
+              <option value="photo">PhotoRealistic</option>
+              <option value="anime">Anime</option>
+              <option value="ghibli">Studio Ghibli</option>
+              <option value="neon">Neon</option>
+            </select>
+          </div>
+
+          <br />
+          <div>
+            <label htmlFor="">Color Scheme: </label>
+            <select name="" id=""></select>
+          </div>
+
+          <br />
+
+          <div>
+            <label htmlFor="">Perspective: </label>
+            <select name="" id=""></select>
+          </div>
+
+          <br />
+
+          <div>
+            <label htmlFor="">Mood: </label>
+            <select name="" id=""></select>
+          </div>
+
+          <br />
+
+          <div>
+            <label htmlFor="">Light: </label>
+            <select name="" id=""></select>
+          </div>
+
+          <br />
+
+          <div>
+            <button>Generate</button>
+          </div>
+        </div>
+
+        <div className="readytextarea-div">
+          <textarea
+            name=""
+            placeholder="Your prompt would appear here."
+            id=""
+            rows={10}
+            cols={50}
+          ></textarea>
+
+          <br />
+
+          <div>
+            <button>Save to buffer</button>
+          </div>
+        </div>
       </div>
-      <br />
-      <div>
-        <label htmlFor="">Choose Model: </label>
-        <select name="" id="">
-          <option value="midjourney">Midjourney</option>
-          <option value="comfyui">ComfyUI</option>
-          <option value="dalle">DALL-E</option>
-        </select>
-      </div>
-      <br />
-      <div>
-        <label htmlFor="">Choose Style: </label>
-        <select name="" id="">
-          <option value="photo">PhotoRealistic</option>
-          <option value="anime">Anime</option>
-          <option value="ghibli">Studio Ghibli</option>
-          <option value="neon">Neon</option>
-        </select>
-      </div>
-      <br />
-      <div>
-        <label htmlFor="">Choose Color Scheme: </label>
-        <select name="" id=""></select>
-      </div>
-      <br />
-      <div>
-        <label htmlFor="">Choose Perspective:</label>
-        <select name="" id=""></select>
-      </div>
-      <br />
-      <div>
-        <label htmlFor="">Mood: </label>
-        <select name="" id=""></select>
-      </div>
-      <br />
-      <div>
-        <label htmlFor="">Light</label>
-      </div>
-      <br />
-      <div>
-        <textarea name="" id="" rows={10} cols={50}></textarea>
-      </div>
-    </div>
+    </section>
   );
 }
 
